67c066ab5ffe7ea7827ce8a28d36db8b7b019b7a
Gabriel Pichot <gabriel.pichot@gmail.com>
2014-04-09 01:51:47 +0200

    Link are not displayed safely

--- lulz/cats/templatetags/pussies.py
+++ lulz/cats/templatetags/pussies.py
@@ -2,13 +2,18 @@ import re
 
 from django import template
 from django.utils.safestring import mark_safe
+from django.utils.translation import ugettext_lazy as _
 
 
 register = template.Library()
 
 @register.filter(name="parse_links")
 def parse_links(text):
-    # make it safe
-    text = re.sub(r'((http|ftp)s?://\S+)', r'<a href="\1">\1</a>', text)
+    text = text.replace('&', '&amp;')
+    text = text.replace('<', '&lt')
+    text = text.replace('>', '&gt')
+    text = text.replace('"', '&quot;')
+    text = re.sub(r'((http|ftp)s?://\S+)', r'<a href="\1">[%s]</a>', text)
+    text = text % _('Link')
 
     return mark_safe(text)
